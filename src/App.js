7328df69c309ebfc716ec9778937b43151a1f4d1
YeMaoteng <32258494+yemaoteng@users.noreply.github.com>
2018-08-13 13:55:04 +0800

    解决问题。能用，完成。准备复习复习再默写

--- src/App.js
+++ src/App.js
@@ -2,33 +2,6 @@ import React from 'react'
 import AddColorForm from './AddColorForm'
 import ColorList from './ColorList'
 import { v4 } from 'uuid'
-//import PropTypes from 'prop-types'
-
-const logColor = (title,color) => {
-    console.log(`TODO: add new ${title} and ${color} to the list`)
-    console.log(`TODO: render UI with new Color`)
-}
-
-var colors = [
-        {
-            "id": "0175d1f0-a8c6-41bf-8d02-df5734d829a4",
-            "title": "coean at dusk",
-            "color": "#26ac56",
-            "rating": 5
-        },
-        {
-            "id": "83c7ba2f-7392-4d7d-9e23-35adbe186046",
-            "title": "lawn",
-            "color": "#26ac56",
-            "rating": 3
-        },
-        {
-            "id": "a11e3995-b0bd-4d58-8c48-5e49ae7f7f23",
-            "title": "bright red",
-            "color": "#ff0000",
-            "rating": 0
-        }
-    ]
 
 class App extends React.Component {
     constructor(props) {
@@ -70,14 +43,10 @@ class App extends React.Component {
         return (
         <div className="app">
             <AddColorForm onNewColor={addColor} />
-            <ColorList colors={colors} rateColor={rateColor} removeColor={removeColor} />
+            <ColorList colors={colors} onRate={rateColor} onRemove={removeColor} />
         </div>
         )
     }
-}
-
-App.propTypes = {
-
 }
 
 export default App
