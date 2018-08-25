c982d062e2eaa67f757bc3dcef3658b478fb5882
YeMaoteng <32258494+yemaoteng@users.noreply.github.com>
2018-08-26 07:51:44 +0800

    陆续新建

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
+import {render} from 'react-dom'
+import {Provider} from 'react-redux'
+import App from './components/App'
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+render(
+    <Provider>
+        <App />
+    </Provider>
+    ,
+    document.getElementById('root')
+)
