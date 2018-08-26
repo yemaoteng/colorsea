8d6dd4fa459dc40d9867a65b1026f7f2c79558d3
YeMaoteng <32258494+yemaoteng@users.noreply.github.com>
2018-08-26 13:00:38 +0800

    增加颜色貌似可以的

--- src/components/ui/AddColorForm.js
+++ src/components/ui/AddColorForm.js
@@ -1,6 +1,37 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-const AddColorForm = () => <h1>Add</h1>
+const AddColorForm = ({onNewColor=f=>f}) => {
+    let _title, _color
+
+    const submit = e => {
+        e.preventDefault()
+        onNewColor(_title.value,_color.value)
+        _title.value = ''
+        _color.value = '#333333'
+        _title.focus()
+    }
+
+    return (
+        <form className="add-color" onSubmit={submit}>
+            <input
+                ref={input=>_title = input}
+                type="text"
+                placeholder="color title..." 
+                required
+                />
+            <input 
+                ref={input=>_color=input}
+                type="color"
+                required 
+                />
+            <button>ADD</button>
+        </form>
+    )
+}
+
+AddColorForm.propTypes = {
+    onNewColor: PropTypes.func
+}
 
 export default AddColorForm
