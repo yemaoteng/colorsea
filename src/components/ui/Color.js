4371cde73892c86f8d6986390d80f4ff6798546a
YeMaoteng <32258494+yemaoteng@users.noreply.github.com>
2018-08-27 09:48:14 +0800

    解决不能删除颜色的BUG。现在排序不能显示问题依然存在。dispatch正常，就是显示不正常

--- src/components/ui/Color.js
+++ src/components/ui/Color.js
@@ -3,9 +3,9 @@ import StarRating from './StarRating'
 import PropTypes from 'prop-types'
 
 
-const Color = ({ title,color,rating,tiemstamp, onRemvoe=f=>f, onRate=f=>f}) => 
+const Color = ({ title,color,rating,tiemstamp, onRemove=f=>f, onRate=f=>f}) => 
     <div className="colorcard">
-        <button onClick={onRemvoe} className="removebtn">X</button>
+        <button onClick={onRemove} className="removebtn">X</button>
         <h3>{title}</h3>
         <p>{rating}</p>
         <p>{color}</p>
@@ -18,7 +18,7 @@ Color.propTypes = {
     title: PropTypes.string.isRequired,
     color: PropTypes.string.isRequired,
     rating: PropTypes.number,
-    onRemove: PropTypes.func,
+    onRemove: PropTypes.func.isRequired,
     onRate: PropTypes.func
 }
 
